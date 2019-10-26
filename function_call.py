d2ffd1c1a1b57101dfdd6a7bd5c77cc6638d2bec
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2019-10-26 21:17:07 +0100

    Make the functions run every day at 18:30
    Schedule the functions to start every day at 18:30.

--- function_call.py
+++ function_call.py
@@ -2,18 +2,28 @@
 import os
 import py_to_sql
 import arcive_files
+import schedule
 
 # To call this functions you need to have MySQL installed and have a
 # database named 'info_3'. The password for the MySQL connection has to be
 # changed to your MySQL server password
 
-## Create a table
-py_to_sql.create_mysql_table()
+def call_all():
+    ## Create a table
+    py_to_sql.create_mysql_table()
 
-## Fill data into the table
-py_to_sql.multiple_files()
+    ## Fill data into the table
+    py_to_sql.multiple_files()
 
-## Move files to archive
-os.chdir('..')
-archive_files.move_to_archive('today\'s_articles', 'archive')
+    ## Move files to archive
+    os.chdir('..')
+    archive_files.move_to_archive('today\'s_articles', 'archive')
 
+schedule.every().day.at('18:30').do(call_all)   
+
+while True:
+    schedule.run_pending()
+
+    
+    
+    
